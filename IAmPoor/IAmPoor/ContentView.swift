c10fb5f78ee4bb95617b5a4484bbff087314dbec
SubhamDrive <subham@driveu.in>
2020-07-19 17:15:23 +0530

    Added Design for my first app

---
+++ IAmPoor/IAmPoor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IAmPoor
+//
+//  Created by Shivam Maheshwari on 19/07/20.
+//  Copyright © 2020 Subham. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
